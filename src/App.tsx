8e9b5a91ef48b9520019d0932807cea84634643f
Air128731 <henry.scott0x8080@gmail.com>
2024-01-09 01:50:27 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+function App() {
+  return (
+    <>
+      <div  className="bg-slate-950 ">
+        <NavBar/>
+        <Body/>
+      </div>
+    </>
+  );
+}
+
+export default App;
